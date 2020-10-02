879b9dc1739c90d6f16db6c8ad99b57be4b2ae11
klismanmelo <61260271+klismanmelo@users.noreply.github.com>
2020-10-01 23:37:34 -0300

    Estilizando página de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import GlobalStyle from './style/globals';
+import SignIn from './pages/SingIn';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello Word</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
